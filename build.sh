1858669ba4213fcd947b48429bf1d9ec5930b183
c1982 <aspsrc@gmail.com>
2022-10-10 16:31:06 +0300

    chore: gitignore update
    * added arm64 build

--- build.sh
+++ build.sh
@@ -1,1 +1,1 @@
-${HOME}/go/bin/gox -osarch="linux/amd64 windows/amd64 darwin/amd64" -output="swed_{{.OS}}_{{.Arch}}" -ldflags="-s -w" ./app/swed
+${HOME}/go/bin/gox -osarch="linux/amd64 windows/amd64 darwin/amd64 darwin/arm64" -output="swed_{{.OS}}_{{.Arch}}" -ldflags="-s -w" ./app/swed
